5739d3ace9aaee9ade52971c4155429373d00157
Lakshan-K <lakshan67@hotmail.com>
2023-11-27 14:49:06 -0500

    fetch pokemon info using pokeapi.co

---
+++ pokedex.js
@@ -0,0 +1,33 @@
+
+const pokemonCount = 350;
+var pokedex = {}; // {1 : {name : "pokemon name", img, url, type : {type, type, type}, description : ...}}
+
+window.onload = async function() {
+    getPokemon(1);
+
+    for(let i = 1; i<= pokemonCount; i++){
+        await getPokemon(i);
+    }
+
+    console.log(pokedex);
+}
+
+async function getPokemon(num){
+    let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+
+    let res = await fetch(url);
+    let pokemon = await res.json();
+    //console.log(pokemon);
+
+    let pokeName = pokemon["name"];
+    let pokeType = pokemon["types"];
+    let pokeImg = pokemon["sprites"]["front_default"];
+
+    res = await fetch(pokemon["species"]["url"]);
+    let pokeDesc = await res.json();
+
+    //console.log(pokeDesc);
+    pokeDesc = pokeDesc["flavor_text_entries"][9]["flavor_text"];
+
+    pokedex[num] = {"name" : pokeName, "img" : pokeImg, "types" : pokeType, "desc" : pokeDesc};
+}
